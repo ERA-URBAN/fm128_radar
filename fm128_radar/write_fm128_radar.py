5c762d2aab2ac8259db79dcadb066b2097b49c90
Ronald van Haren <r.vanharen@esciencecenter.nl>
2017-02-07 13:20:26 +0100

    add support for specific lon/lat grid for each radar elevation angle

--- fm128_radar/write_fm128_radar.py
+++ fm128_radar/write_fm128_radar.py
@@ -10,7 +10,11 @@ import numpy
 class write_fm128_radar:
   def __init__(self, radar_name, lat0, lon0, elv0, date, lat,
                lon, elv, rf, rf_qc, rf_err,
-               rv, rv_qc, rv_err, outfile='fm128_radar.out'):
+               rv, rv_qc, rv_err, outfile='fm128_radar.out', single=True):
+    '''
+    single: boolean indicating if each reflection angle has its own distinct
+    lon/lat grid
+    '''
     if (numpy.shape(radar_name) == True and numpy.shape(radar_name)>1):
       # multiple radars in output file
       nrad = numpy.shape(radar_name)
@@ -18,23 +22,40 @@ class write_fm128_radar:
       dstring = [d.strftime('%Y-%m-%d %H:%M:%S') for d in date]
       self.init_file(nrad, outfile)
       for r_int in range(0, nrad):
-        max_levs = numpy.shape(elv[r_int])[0]
+        if single:
+          max_levs = numpy.shape(elv[r_int])[0]
+        else:
+          max_levs = numpy.ones(nrad)
         np = self.get_number_of_points(rf[r_int])
         # number of points: degrees * distance
         self.write_header(radar_name[r_int], lon0[r_int], lat0[r_int],
-                          elv0[r_int], dstring[r_int], np[r_int], max_levs[r_int])
-        self.write_data(dstring[r_int], lat[r_int], lon[r_int], elv0[r_int],
-                        elv[r_int], rv[r_int], rv_qc[r_int],
-                        rv_err[r_int], rf[r_int], rf_qc[r_int], rf_err[r_int])
+                          elv0[r_int], dstring[r_int], np[r_int],
+                          max_levs[r_int])
+        if single:
+          self.write_data_single(dstring[r_int], lat[r_int], lon[r_int],
+                                 elv0[r_int], elv[r_int], rv[r_int],
+                                 rv_qc[r_int], rv_err[r_int], rf[r_int],
+                                 rf_qc[r_int], rf_err[r_int])
+        else:
+          self.write_data(dstring[r_int], lat[r_int], lon[r_int], elv0[r_int],
+                          elv[r_int], rv[r_int], rv_qc[r_int],
+                          rv_err[r_int], rf[r_int], rf_qc[r_int], rf_err[r_int])
     else:
       # one radar in output file
       self.init_file(1, outfile)
-      max_levs = numpy.shape(elv)[0]
+      if single:
+        max_levs = numpy.shape(elv)[0]
+      else:
+        max_levs = 1
       np = self.get_number_of_points(rf)
       dstring = date.strftime('%Y-%m-%d %H:%M:%S')
       self.write_header(radar_name, lon0, lat0, elv0, dstring, np, max_levs)
-      self.write_data(dstring, lat, lon, elv0, elv, rv, rv_qc, rv_err,
-                      rf, rf_qc, rf_err)
+      if single:
+        self.write_data_single(dstring, lat, lon, elv0, elv, rv, rv_qc, rv_err,
+                               rf, rf_qc, rf_err)
+      else:
+        self.write_data(dstring, lat, lon, elv0, elv, rv, rv_qc, rv_err,
+                        rf, rf_qc, rf_err)
     self.close_file()
 
   def init_file(self, nrad, outfile):
@@ -106,10 +127,50 @@ class write_fm128_radar:
     try:
       return rf_data_point.count()
     except AttributeError:
-      return len(rf_data_point)
+      try:
+        return len(rf_data_point)
+      except TypeError:
+        return 1
 
   def write_data(self, date, lat, lon, elv0, elv, rv_data, rv_qc, rv_err,
                  rf_data, rf_qc, rf_err):
+    '''
+    Write radar measurements to the output file
+    '''
+    fmt = "%12s%3s%19s%2s%12.3f%2s%12.3f%2s%8.1f%2s%6i"
+    fmt_2 = "%3s%12.1f%12.3f%4i%12.3f%2s%12.3f%4i%12.3f%2s"
+    hor_spacing = ''
+    # loop over horizontal data points
+    for m in range(0, numpy.shape(lat)[0]):  # vertical levels
+      for i in range(0, numpy.shape(lat)[1]):
+        for j in range(0, numpy.shape(lat)[2]):
+          levs = self.get_levs_point(rf_data[m,i,j])
+          if levs > 0:
+            # Only write the output data if there is at least 1 vertical level
+            # with reflectivity data
+            self.f.write(fmt % ('FM-128 RADAR', hor_spacing, date, hor_spacing,
+                                lat[m,i,j], hor_spacing, lon[m,i,j], hor_spacing,
+                                elv0, hor_spacing, levs))
+            self.f.write("\n")
+            # loop over vertical elevations for each radar
+            try:
+              if not rf_data.mask[m,i,j]:
+                self.f.write(fmt_2 % (hor_spacing, elv[m,i,j],
+                                      rv_data[m,i,j], rv_qc[m,i,j],
+                                      rv_err[m,i,j], hor_spacing,
+                                      rf_data[m,i,j], rf_qc[m,i,j],
+                                      rf_err[m,i,j], hor_spacing))
+                self.f.write("\n")
+            except AttributeError:
+              self.f.write(fmt_2 % (hor_spacing, elv[m,i,j],
+                                    rv_data[m,i,j], rv_qc[m,i,j],
+                                    rv_err[m,i,j], hor_spacing,
+                                    rf_data[m,i,j], rf_qc[m,i,j],
+                                    rf_err[m,i,j], hor_spacing))
+              self.f.write("\n")
+
+  def write_data_single(self, date, lat, lon, elv0, elv, rv_data, rv_qc, rv_err,
+                        rf_data, rf_qc, rf_err):
     '''
     Write radar measurements to the output file
     '''
